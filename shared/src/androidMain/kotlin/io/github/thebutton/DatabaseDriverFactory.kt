050265c93f8aca78fd9ea1bf7eec8f1d4e4034f8
Lo_op <shaikhfarhaan312@gmail.com>
2025-06-25 18:42:34 -0400

    minor fixes

--- shared/src/androidMain/kotlin/io/github/thebutton/DatabaseDriverFactory.kt
+++ shared/src/androidMain/kotlin/io/github/thebutton/DatabaseDriverFactory.kt
@@ -3,7 +3,6 @@ package io.github.thebutton
 import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
-import io.github.thebutton.cache.AppDatabase
 
 actual class DriverFactory(private val context: Context) {
     actual fun createDriver(): SqlDriver {
--- shared/src/iosMain/kotlin/io/github/thebutton/DatabaseDriverFactory.kt
+++ shared/src/iosMain/kotlin/io/github/thebutton/DatabaseDriverFactory.kt
@@ -2,8 +2,6 @@ package io.github.thebutton
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import io.github.thebutton.cache.AppDatabase
-import io.github.thebutton.DriverFactory
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
